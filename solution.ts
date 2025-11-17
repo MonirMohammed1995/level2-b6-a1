70752408ac0a520ff09ac1014db7424a0b3412c9
MonirMohammed1995 <monirmdnayemsaju42@gmail.com>
2025-11-17 17:48:02 +0600

    Problem 4 and 5 solved

--- solution.ts
+++ solution.ts
@@ -8,26 +8,93 @@ function formatValue(value: string | number | boolean) {
   return !value;
 }
 
+
 function getLength(value: string | any[]) {
   if (typeof value === "string") {
     return value.length;
   }
-
   if (Array.isArray(value)) {
     return value.length;
   }
 }
 
+
 class Person {
   name: string;
   age: number;
-
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
-
   getDetails() {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
+
+
+type Item = {
+  title: string;
+  rating: number;
+};
+function filterByRating(items: Item[]): Item[] {
+  const result: Item[] = [];
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].rating >= 4) {
+      result.push(items[i]);
+    }
+  }
+  return result;
+}
+
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[] {
+  const activeUsers: User[] = [];
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].isActive) {
+      activeUsers.push(users[i]);
+    }
+  }
+
+  return activeUsers;
+}
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book) {
+  let availableText = book.isAvailable ? "Yes" : "No";
+  console.log(
+    "Title: " + book.title +
+    ", Author: " + book.author +
+    ", Published: " + book.publishedYear +
+    ", Available: " + availableText
+  );
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: false,
+};
+
+printBookDetails(myBook);
